9fe43334cd940d6716ec2218d4a20c44cf8c285b
Jaliyagoone <jaliya_08@live.com>
2023-06-01 22:37:44 +0100

    Finished lesson

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ const scene = new Scene();
 
 //2 The object
 const geometry = new BoxGeometry(0.5,0.5,0.5);
-const material = new MeshBasicMaterial({color: 'orange'});
-const mesh = new Mesh(geometry, material);
+const orangeMaterial = new MeshBasicMaterial({color: 'orange'});
+const blueMaterial = new MeshBasicMaterial({color: 'blue'});
 
-scene.add(mesh);
+
+const orangeCube = new Mesh(geometry, orangeMaterial);
+scene.add(orangeCube);
+
+const bigOrangeCube = new Mesh(geometry, blueMaterial);
+bigOrangeCube.position.x += 2;
+bigOrangeCube.scale.set(2,2,2)
+scene.add(bigOrangeCube);
+
 
 // 3 Camera
 const sizes = {
@@ -31,4 +39,16 @@ scene.add(camera);
 const canvas = document.getElementById('three-canvas');
 const renderer = new WebGLRenderer({canvas});
 renderer.setSize(sizes.width,sizes.height);
-renderer.render(scene, camera);
+
+function animate(){
+    orangeCube.rotation.x += 0.01;
+    orangeCube.rotation.z += 0.01;
+
+    bigOrangeCube.rotation.x += 0.02;
+    bigOrangeCube.rotation.z += 0.02;
+
+    renderer.render(scene, camera);    
+    requestAnimationFrame(animate);
+}
+
+animate();
