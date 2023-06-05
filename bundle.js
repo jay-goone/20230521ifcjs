c40dde73820d8f071e8cca218f3576975f6fda97
Jaliyagoone <jaliya_08@live.com>
2023-06-05 22:06:05 +0100

    Rendering added

--- bundle.js
+++ bundle.js
@@ -29261,6 +29261,7 @@ if ( typeof window !== 'undefined' ) {
 
 // 1 Scene
 const scene = new Scene();
+const canvas = document.getElementById('three-canvas');
 
 //2 The object
 const geometry = new BoxGeometry(0.5,0.5,0.5);
@@ -29278,19 +29279,26 @@ scene.add(bigOrangeCube);
 
 
 // 3 Camera
-const sizes = {
-    width: 800,
-    height: 600
-};
 
-const camera = new PerspectiveCamera(75, sizes.width/sizes.height);
+
+const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight);
 camera.position.z = 3;
 scene.add(camera);
 
 // 4 The Renderer
-const canvas = document.getElementById('three-canvas');
+
 const renderer = new WebGLRenderer({canvas});
-renderer.setSize(sizes.width,sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+renderer.setSize(canvas.clientWidth,canvas.clientHeight, false);
+
+// 5 Responsivity
+window.addEventListener('resize', () => {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+});
+
+//6 Animation
 
 function animate(){
     orangeCube.rotation.x += 0.01;
